6ee5b1cc41579c63a4de336b701b518af35e5cb2
Brian Balser <brian.balser@gmail.com>
2016-05-08 18:18:44 -0700

    more dsl design

--- src/main/kotlin/evercraft/equippable/Equippable.kt
+++ src/main/kotlin/evercraft/equippable/Equippable.kt
@@ -1,22 +1,70 @@
 package evercraft.equippable
 
 import evercraft.AbilitiesBuilder
+import evercraft.Character
+import java.util.concurrent.atomic.AtomicInteger
 import kotlin.properties.Delegates
 
 open class Equipable(block: EquipableBody.() -> Unit) {
-    val body = EquipableBody()
+    private var data: MutableMap<String, Int> = mutableMapOf()
+
+    val strength: Int by data
+    val dexterity: Int by data
 
     init {
+        val body = EquipableBody()
         body.block()
+        body.build(data)
+    }
+
+    fun attackBonus(attacker: Character, defender: Character):Int {
+        TODO()
     }
 
 }
 
 class EquipableBody {
 
-    val abilitiesBuilder = AbilitiesBuilder()
+    val abilitiesBonusBuilder = AbilitiesBonusBuilder()
 
-    fun abilities(block: AbilitiesBuilder.() -> Unit): Unit = abilitiesBuilder.block()
+    fun abilities(block: AbilitiesBonusBuilder.() -> Unit): Unit = abilitiesBonusBuilder.block()
+
+    class AbilitiesBonusBuilder {
+
+        val strength: IntHolder = IntHolder(0)
+        val dexterity: IntHolder = IntHolder(0)
+
+    }
+
+    fun whenAttacking(block: WhenAttackingBody.() -> Unit): Unit = {
+        
+    }
+
+    internal fun build(data: MutableMap<String, Int>): Unit {
+        data["strength"] = abilitiesBonusBuilder.strength.value
+        data["dexterity"] = abilitiesBonusBuilder.dexterity.value
+    }
+}
+
+class WhenAttackingBody {
+
+    val attack: MutableList<(evercraft.Character, evercraft.Character) -> Unit> = mutableListOf()
+
+    operator fun <T> MutableList<T>.plus(other :T): Unit {
+        add(other)
+    }
 
 
 }
+
+
+class IntHolder(initialValue: Int) {
+    var value: Int = initialValue
+
+    operator fun plus(other: Int): Unit {
+        value = value + other
+    }
+
+}
+
+
--- src/test/kotlin/evercraft/equippable/EquippableSpec.kt
+++ src/test/kotlin/evercraft/equippable/EquippableSpec.kt
@@ -1,16 +1,38 @@
 package evercraft.equippable
 
+import evercraft.Alignment
 import org.jetbrains.spek.api.Spek
+import kotlin.test.assertEquals
 
-class EquippableSpec: Spek({
+class EquippableSpec : Spek({
 
-    val equipable = object: Equipable({
+    given("A test equipable") {
 
-        abilities {
-            strength + 1
-        }    
+        on("when created with specified values") {
+        val equipable = object : Equipable({
 
-    })
+            abilities {
+                strength + 1
+                dexterity + 2
+            }
+
+            whenAttacking {
+                attack + { attacker, defender -> if (defender.alignment == Alignment.EVIL) 2 else 0 }
+            }
+
+        }) {}
+
+            it("should have a strength of 1") {
+                assertEquals(1, equipable.strength)
+            }
+
+            it("should have a dexterity of 1") {
+                assertEquals(2, equipable.dexterity)
+            }
+        }
+
+
+    }
 
 
 })
